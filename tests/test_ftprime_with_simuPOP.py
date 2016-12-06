5d59d7c1fcf7d8fc022cbc3694369bb160fb3a94
Jaime Ashander <jashander@ucdavis.edu>
2016-12-05 23:30:15 -0800

    Wrap test in functions to run in pytest
    Only the WF test really checks output/calcs;
    simuPOP test just runs the example code.
    
    test_ftprime_with_wf.py ....
    
    ----------- coverage: platform linux, python 3.5.2-final-0 -----------
    Name                                             Stmts   Miss  Cover   Missing
    ------------------------------------------------------------------------------
    /home/jaime/lib/ftprime/ftprime/__init__.py          2      0   100%
    /home/jaime/lib/ftprime/ftprime/meiosis.py          47     33    30%   11, 15-18, 23, 27, 31-37, 43-49, 59-75
    /home/jaime/lib/ftprime/ftprime/pedrecorder.py      69      7    90%   13, 21, 32, 70, 72, 113-115
    ------------------------------------------------------------------------------
    TOTAL                                              118     40    66%
    
    =========================== 4 passed in 0.18 seconds ===========================

--- tests/test_ftprime_with_simuPOP.py
+++ tests/test_ftprime_with_simuPOP.py
@@ -8,68 +8,69 @@ simuOpt.setOptions(optimized=False, debug='DBG_WARNING')
 # sim.turnOnDebug('DBG_POPULATION,DBG_INDIVIDUAL')
 sim.setOptions(seed=111)
 
-popSize=5
-generations=3
-replications=1
-nsamples=5
+def test_simupop_runs():
+    popSize=5
+    generations=3
+    replications=1
+    nsamples=5
 
-meioser=MeiosisTagger(nsamples,ngens=1+generations)
-def step_gen(pop,param):
-    # function to increment internal clock in MeiosisTagger
-    # as in http://simupop.sourceforge.net/manual_svn/build/userGuide_ch5_sec14.html#python-operator-pyoperator
-    dt,=param
-    print("Time was", meioser.gen," and now is ",meioser.gen+dt)
-    meioser.gen+=dt
-    return True
+    meioser=MeiosisTagger(nsamples,ngens=1+generations)
+    def step_gen(pop,param):
+        # function to increment internal clock in MeiosisTagger
+        # as in http://simupop.sourceforge.net/manual_svn/build/userGuide_ch5_sec14.html#python-operator-pyoperator
+        dt,=param
+        print("Time was", meioser.gen," and now is ",meioser.gen+dt)
+        meioser.gen+=dt
+        return True
 
-reproduction = sim.RandomMating(
-    ops=[
-        # sim.IdTagger(),                   # new ID for offspring
-        sim.PyTagger(func=meioser.new_offspring),
-        # PyTagger operates on infofields defined by names of the arguments of func
-    ]
-)
+    reproduction = sim.RandomMating(
+        ops=[
+            # sim.IdTagger(),                   # new ID for offspring
+            sim.PyTagger(func=meioser.new_offspring),
+            # PyTagger operates on infofields defined by names of the arguments of func
+        ]
+    )
 
-# note that 'pop' is a temporary object.
-pop = sim.Population(size=popSize, infoFields=['ind_id'])
-simu = sim.Simulator(pop)
+    # note that 'pop' is a temporary object.
+    pop = sim.Population(size=popSize, infoFields=['ind_id'])
+    simu = sim.Simulator(pop)
 
-simu.evolve(
-    # everyone initially will have the same allele frequency
-    initOps = [
-        sim.InitSex(),
-        # sim.IdTagger(),
-        # sim.PyOperator(func=init_labels),
-        meioser
-    ],
-    preOps = sim.PyOperator(func=step_gen,param=(1,)),
-    matingScheme = reproduction,
-    gen = generations
-)
+    simu.evolve(
+        # everyone initially will have the same allele frequency
+        initOps = [
+            sim.InitSex(),
+            # sim.IdTagger(),
+            # sim.PyOperator(func=init_labels),
+            meioser
+        ],
+        preOps = sim.PyOperator(func=step_gen,param=(1,)),
+        matingScheme = reproduction,
+        gen = generations
+    )
 
-# temporary reference to 0-th replicate population
-pop=simu.population(0)
-pop_ids = [ ind.info('ind_id') for ind in pop.individuals() ]
+    # temporary reference to 0-th replicate population
+    pop=simu.population(0)
+    pop_ids = [ ind.info('ind_id') for ind in pop.individuals() ]
 
-print("final individuals:")
-for ind in pop.individuals():
-    print("id: ", ind.info('ind_id'))
+    print("final individuals:")
+    for ind in pop.individuals():
+        print("id: ", ind.info('ind_id'))
 
-samples=random.sample(pop_ids,nsamples)
-# need chromosome ids
-chrom_samples = [ ind_to_chrom(x,a) for x in samples for a in mapa_labels ]
-times=[ meioser.time[x] for x in samples for a in mapa_labels ]
-meioser.records.add_samples(samples=chrom_samples,times=times,populations=[0 for x in chrom_samples])
+    samples=random.sample(pop_ids,nsamples)
+    # need chromosome ids
+    chrom_samples = [ ind_to_chrom(x,a) for x in samples for a in mapa_labels ]
+    times=[ meioser.time[x] for x in samples for a in mapa_labels ]
+    meioser.records.add_samples(samples=chrom_samples,times=times,populations=[0 for x in chrom_samples])
 
-for x in meioser.records.dump_records():
-    print(x)
+    for x in meioser.records.dump_records():
+        print(x)
 
-# print("Python trees:")
-# for t in trees(list(meioser.records.dump_records())):
-#     print(t)
+    # print("Python trees:")
+    # for t in trees(list(meioser.records.dump_records())):
+    #     print(t)
 
-print("msprime trees:")
-ts=meioser.records.tree_sequence()
+    print("msprime trees:")
+    ts=meioser.records.tree_sequence()
 
-for t in ts.trees():
-    print(t)
+    for t in ts.trees():
+        print(t)
--- tests/test_ftprime_with_wf.py
+++ tests/test_ftprime_with_wf.py
@@ -2,68 +2,68 @@ from ftprime import merge_records
 import msprime
 from wf import wf
 
-# test 1
+def test_1():
 
-new_rec=msprime.CoalescenceRecord(left=0.8, right=1.0, node=18, children=(22,), time=2, population=0)
-old_recs=[
+    new_rec=msprime.CoalescenceRecord(left=0.8, right=1.0, node=18, children=(22,), time=2, population=0)
+    old_recs=[
+            msprime.CoalescenceRecord(left=0.0, right=0.6, node=18, children=(19,), time=2, population=0),
+            msprime.CoalescenceRecord(left=0.6, right=1.0, node=18, children=(19, 20), time=2, population=0)]
+    merge_records(new_rec,old_recs)
+
+    old_recs
+
+    right_answer=[
         msprime.CoalescenceRecord(left=0.0, right=0.6, node=18, children=(19,), time=2, population=0),
-        msprime.CoalescenceRecord(left=0.6, right=1.0, node=18, children=(19, 20), time=2, population=0)]
-merge_records(new_rec,old_recs)
+        msprime.CoalescenceRecord(left=0.6, right=0.8, node=18, children=(19,20), time=2, population=0),
+        msprime.CoalescenceRecord(left=0.8, right=1.0, node=18, children=(19, 20, 22), time=2, population=0),
+        ]
 
-old_recs
+    if not all([ a==b for a,b in zip(right_answer,old_recs) ]):
+        raise ValueError('whoops')
 
-right_answer=[
-    msprime.CoalescenceRecord(left=0.0, right=0.6, node=18, children=(19,), time=2, population=0),
-    msprime.CoalescenceRecord(left=0.6, right=0.8, node=18, children=(19,20), time=2, population=0),
-    msprime.CoalescenceRecord(left=0.8, right=1.0, node=18, children=(19, 20, 22), time=2, population=0),
-    ]
+def test_2():
 
-if not all([ a==b for a,b in zip(right_answer,old_recs) ]):
-    raise ValueError('whoops')
+    new_rec = msprime.CoalescenceRecord(left=0.5, right=1.0, node=4, children=(13,), time=6, population=0)
+    old_recs = [ msprime.CoalescenceRecord(left=0.0, right=0.9, node=4, children=(7, 12), time=6, population=0),
+                 msprime.CoalescenceRecord(left=0.9, right=1.0, node=4, children=(7,), time=6, population=0) ]
+    merge_records(new_rec,old_recs)
 
-# test 2
+    right_answer=[
+        msprime.CoalescenceRecord(left=0.0, right=0.5, node=4, children=(7, 12), time=6, population=0),
+        msprime.CoalescenceRecord(left=0.5, right=0.9, node=4, children=(7, 12, 13), time=6, population=0),
+        msprime.CoalescenceRecord(left=0.9, right=1.0, node=4, children=(7, 13), time=6, population=0)
+        ]
 
-new_rec = msprime.CoalescenceRecord(left=0.5, right=1.0, node=4, children=(13,), time=6, population=0)
-old_recs = [ msprime.CoalescenceRecord(left=0.0, right=0.9, node=4, children=(7, 12), time=6, population=0),
-             msprime.CoalescenceRecord(left=0.9, right=1.0, node=4, children=(7,), time=6, population=0) ]
-merge_records(new_rec,old_recs)
+    if not all([ a==b for a,b in zip(right_answer,old_recs) ]):
+        raise ValueError('whoops')
 
-right_answer=[
-    msprime.CoalescenceRecord(left=0.0, right=0.5, node=4, children=(7, 12), time=6, population=0),
-    msprime.CoalescenceRecord(left=0.5, right=0.9, node=4, children=(7, 12, 13), time=6, population=0),
-    msprime.CoalescenceRecord(left=0.9, right=1.0, node=4, children=(7, 13), time=6, population=0)
-    ]
+def test_3():
 
-if not all([ a==b for a,b in zip(right_answer,old_recs) ]):
-    raise ValueError('whoops')
+    new_rec = msprime.CoalescenceRecord(left=0.1, right=1.0, node=9, children=(11,), time=4, population=0)
+    old_recs = [msprime.CoalescenceRecord(left=0.0, right=0.1, node=9, children=(11,), time=4, population=0)]
+    merge_records(new_rec,old_recs)
 
-# test 3
+    right_answer=[
+        msprime.CoalescenceRecord(left=0.0, right=0.1, node=9, children=(11,), time=4, population=0),
+        msprime.CoalescenceRecord(left=0.1, right=1.0, node=9, children=(11,), time=4, population=0)
+        ]
 
-new_rec = msprime.CoalescenceRecord(left=0.1, right=1.0, node=9, children=(11,), time=4, population=0)
-old_recs = [msprime.CoalescenceRecord(left=0.0, right=0.1, node=9, children=(11,), time=4, population=0)]
-merge_records(new_rec,old_recs)
-
-right_answer=[
-    msprime.CoalescenceRecord(left=0.0, right=0.1, node=9, children=(11,), time=4, population=0),
-    msprime.CoalescenceRecord(left=0.1, right=1.0, node=9, children=(11,), time=4, population=0)
-    ]
-
-if not all([ a==b for a,b in zip(right_answer,old_recs) ]):
-    raise ValueError('whoops')
+    if not all([ a==b for a,b in zip(right_answer,old_recs) ]):
+        raise ValueError('whoops')
 
 
-#### test simulation runs
+def test_simulation_runs():
 
-records = wf(N=5,ngens=5,nsamples=5,survival=0.5)
+    records = wf(N=5,ngens=5,nsamples=5,survival=0.5)
 
-for x in records.dump_records():
-    print(x)
+    for x in records.dump_records():
+        print(x)
 
-ts = records.tree_sequence()
+    ts = records.tree_sequence()
 
-for t in ts.trees():
-    print(t)
+    for t in ts.trees():
+        print(t)
 
-print("Mean pairwise diversity:",ts.get_pairwise_diversity())
-print("(should be zero)")
-
+    print("Mean pairwise diversity:",ts.get_pairwise_diversity())
+    print("(should be zero)")
+    assert ts.get_pairwise_diversity() == 0.0
